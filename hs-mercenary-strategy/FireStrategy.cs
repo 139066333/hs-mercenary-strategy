742f892b38180a8f2e9c8d11e2c0b6de04ce7a3d
jimowushuang <jimowushuang@outlook.com>
2022-06-18 17:05:36 +0800

    init

---
+++ hs-mercenary-strategy/FireStrategy.cs
@@ -0,0 +1,74 @@
+using System.Collections.Generic;
+
+namespace HsMercenaryStrategy
+{
+    public class FireStrategy: IStrategy
+    {
+        public List<BattleTarget> GetBattleTargets(List<Mercenary> mercenaries, List<Target> targets)
+        {
+
+            var battleTargetEntities = new List<BattleTarget>();
+            
+            // 点火攻击血量最高的怪
+            Mercenary fireUp = StrategyUtils.FindMercenaryForName("安东尼", mercenaries) ??  
+                                     StrategyUtils.FindMercenaryForName("赤精", mercenaries) ??  
+                                     StrategyUtils.FindMercenaryForName("巴琳达", mercenaries);
+            Target maxHealthTarget = StrategyUtils.FindMaxHealthTarget(targets);
+            if (fireUp != null)
+            {
+                battleTargetEntities.Add( new BattleTarget() {
+                    TargetId = maxHealthTarget?.Id ?? -1,
+                    SkillId =  StrategyUtils.FindSkillForName("火球", fireUp)?.Id ?? 
+                               StrategyUtils.FindSkillForName("烈焰之歌", fireUp)?.Id ??
+                               StrategyUtils.FindSkillForName("烈焰之刺", fireUp)?.Id ?? -1
+                });
+            } 
+
+
+            // 嘉顿
+            // ReSharper disable once IdentifierTypo
+            var geddon = StrategyUtils.FindMercenaryForName("迦顿", mercenaries);
+            if (geddon != null)
+            {
+                battleTargetEntities.Add( new BattleTarget() {
+                    SkillId =  StrategyUtils.FindSkillForName("地狱火", geddon)?.Id  ?? -1
+                });
+            }
+
+            // 螺丝
+            // ReSharper disable once IdentifierTypo
+            var ragnaros = StrategyUtils.FindMercenaryForName("拉格纳罗斯", mercenaries);
+                
+            if (ragnaros != null)
+            {
+                battleTargetEntities.Add( new BattleTarget() {
+                    SkillId =  StrategyUtils.FindSkillForName("死吧，虫子", ragnaros)?.Id  ?? -1
+                });
+            }
+        
+            
+            // 如果有水元素，用水元素冻住速度最慢的敌人
+            var water = StrategyUtils.FindMercenaryForName("水元素",mercenaries);
+            if (water != null)
+            {
+                // 找到水元素的技能
+                var attack = StrategyUtils.FindSkillForName("攻击",water);
+
+                // 找到最慢的敌人
+                var slowestTarget = StrategyUtils.FindSlowestTarget(targets);
+                
+                battleTargetEntities.Add( new BattleTarget() {
+                    TargetId = slowestTarget?.Id ?? -1,
+                    SkillId =  attack?.Id ?? -1
+                });
+            }
+            
+            return battleTargetEntities;
+        }
+
+        public string Name()
+        {
+            return "火焰队策略";
+        }
+    }
+}
---
+++ hs-mercenary-strategy/IStrategy.cs
@@ -0,0 +1,96 @@
+﻿using System.Collections.Generic;
+using System.Linq;
+
+namespace HsMercenaryStrategy
+{
+    public interface IStrategy
+    {
+        List<BattleTarget> GetBattleTargets(List<Mercenary> mercenaries,List<Target> targets);
+        
+        string Name();
+    }
+
+    public static class StrategyUtils
+    {
+
+        /**
+         * 根据名称找佣兵
+         */
+        public static Mercenary FindMercenaryForName(string name,List<Mercenary> mercenaries)
+        {
+            return mercenaries.Find(m => m.Name.Contains(name) || name.Contains(m.Name));
+        }
+        
+        /**
+         * 根据名称找技能
+         */
+        public static Skill FindSkillForName(string name,Mercenary mercenary)
+        {
+            return mercenary.Skills.Find(s => s.Name.Contains(name) || name.Contains(s.Name));
+        }
+
+        /**
+         * 找到速度最慢的敌人
+         */
+        public static Target FindSlowestTarget(List<Target> targetEntities)
+        {
+            if (targetEntities.Count == 0)
+            {
+                return null;
+            }
+            var speed = targetEntities.Max(i=>i.Speed);
+            return targetEntities.Find(i => i.Speed == speed);
+        }
+
+        /**
+         * 找到声明最高的敌人
+         */
+        public static Target FindMaxHealthTarget(List<Target> targetEntities)
+        {
+            if (targetEntities.Count == 0)
+            {
+                return null;
+            }
+            var health = targetEntities.Max(i=>i.Health);
+            return targetEntities.Find(i => i.Health == health);
+        }
+    }
+
+    public class Target
+    {
+        public int Id = -1;
+
+        public string Name;
+
+        public int Health;
+
+        public int Speed;
+    }
+
+
+    public class BattleTarget
+    {
+        public int TargetId = -1;
+
+        public int SkillId = -1;
+    }
+
+    public class Mercenary
+    {
+        public int Id = -1;
+
+        public string Name;
+
+        // ReSharper disable once CollectionNeverUpdated.Global
+        public List<Skill> Skills;
+    }
+
+    public class Skill
+    {
+        public int Id = -1;
+        
+        public string Name;
+
+        public int Speed;
+    }  
+}
